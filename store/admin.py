b59eb6a9a84c955edd40001cb9d6e73e3629c53c
xdr1000 <vijaypr1987@gmail.com>
2016-03-17 23:31:09 +0530

    gitingnore updated

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Book, Author
+from .models import Book,BookOrder, Author, Cart
 
 
 
@@ -11,6 +11,13 @@ class BookAdmin(admin.ModelAdmin):
 class AuthorAdmin(admin.ModelAdmin):
     list_display= ('last_name', 'first_name')
 
+class BookOrderAdmin(admin.ModelAdmin):
+    list_display= ('book', 'cart','quantity')
+
+class CartAdmin(admin.ModelAdmin):
+    list_display= ('user', 'active','order_date')
+
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author, AuthorAdmin)
-
+admin.site.register(BookOrder, BookOrderAdmin)
+admin.site.register(Cart, CartAdmin)
